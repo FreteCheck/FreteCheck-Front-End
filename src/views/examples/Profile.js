32503e3113a15018ec6703462d2a18ac9f25da06
Vagner Ferreira Lima Junior <116043257+vagnerflj@users.noreply.github.com>
2025-04-07 20:21:24 -0300

    feat:(NB-02)Mudanças no Layout do Projeto

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -97,7 +97,7 @@ const Profile = () => {
                 </Row>
                 <div className="text-center">
                   <h3>
-                    Jessica Jones
+                    Vagner Ferreira
                     <span className="font-weight-light">, 27</span>
                   </h3>
                   <div className="h5 font-weight-300">
